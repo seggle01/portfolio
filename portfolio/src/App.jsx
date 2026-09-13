448b58557de852d0e7084f345a4966cfbf74dc4e
Stefanos Englezou <egglezoustefanos@gmail.com>
2026-09-13 12:03:18 +0200

    Removed old components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,29 @@
-// Application component
-import Main from "./components/main"
+// This is a Main Component of including everything else
+import Nav from "./new_components/nav";
+import Intro from "./new_components/intro";
+import Profile from "./new_components/profile";
+import Interests from "./new_components/interests";
+import Projects from "./new_components/projects";
+import Footer from "./new_components/links";
 
-function App() {
-  return (
-    <>
-      <Main/>
-    </>
-  );
+const  main = () =>{
+    return (
+        <>
+            <div className="bg-slate-800">
+                <Nav/>
+            </div>
+
+            <div className="bg-slate-500">
+                <Intro/>
+            </div>
+            
+            <div className="bg-gray-800">
+                <Profile/>
+                <Interests/>
+                <Projects/>
+                <Footer/>
+            </div>
+        </>
+    );
 }
-
-export default App
+export default main
