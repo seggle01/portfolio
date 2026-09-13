6edb00c5530d93e672dd9796ebc74b2e7b839f21
Stefanos Englezou <egglezoustefanos@gmail.com>
2026-09-13 12:27:36 +0200

    launch config

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   server: {
+    // allowedHosts: ['endorphin-refueling-dicing.ngrok-free.dev'],
     host: '0.0.0.0',
     port: 3000,
   },
